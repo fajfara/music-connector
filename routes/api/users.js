ee46bba7b73616bfe452a9ec9278ad2164503f7b
Anže Fajfar <a.fajfar95@gmail.com>
2018-07-13 17:43:39 +0200

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 
 
@@ -53,4 +55,52 @@ router.post('/register', (req, res) => {
         })
 });
 
+// @route   GET api/users/login
+// @desc    Login user / Returning the JWT token
+// @access  Public 
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find the user by email
+    User.findOne({email})
+        .then(user => {
+            // Check for user
+            if(!user){
+                return res.status(404).json({email: 'User not found!'});
+            }
+
+            // Check password 
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        // User Matched
+
+                        // Create jwt payload
+                        const  payload = {
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar
+                        };
+
+                        // Sign token
+                        jwt.sign(
+                            payload, 
+                            keys.secretOrKey, 
+                            { expiresIn: 3600 }, 
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                })
+                        });
+
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'});
+                    }
+                })
+        })
+
+});
 module.exports = router;
