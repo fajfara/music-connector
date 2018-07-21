618d1cccaf112593065533237c18f088e6f8680a
Anže Fajfar <a.fajfar95@gmail.com>
2018-07-21 23:32:49 +0200

    adding profile model

--- models/Profile.js
+++ models/Profile.js
@@ -120,4 +120,4 @@ const ProfileSchema = new Schema({
 
 });
 
-module.exports = Profile = mongoose.models('profile', ProfileSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
