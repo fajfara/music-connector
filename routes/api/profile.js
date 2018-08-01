570529433830e08e18bb6805e25fba81addc9746
Anže Fajfar <a.fajfar95@gmail.com>
2018-08-01 22:16:25 +0200

    Update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load validation 
+const validateProfileInput = require('../../validation/profile');
+
 // Load profile model
 const Profile = require('../../models/Profile');
 // Load user model
@@ -35,6 +38,15 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @desc    Create user profile
 // @access  Private 
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check validation
+    if(!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
     profileFields.User = req.user.id;
