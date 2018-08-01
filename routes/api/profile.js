7e98f2cf785638c7b383e816f524fd73941d2833
Anže Fajfar <a.fajfar95@gmail.com>
2018-08-01 23:08:48 +0200

    minor bug fixing

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -35,24 +35,44 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err => res.status(404).json(err));
 });
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+    const errors = {};
+  
+    Profile.find()
+      .populate('user', ['name', 'avatar'])
+      .then(profiles => {
+        if (!profiles) {
+          errors.noprofile = 'There are no profiles 1';
+          return res.status(404).json(errors);
+        }
+  
+        res.json(profiles);
+      })
+      .catch(err => res.status(404).json({ profile: 'There are no profiles 2' + err }));
+});
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
-// @access  public
+// @access  Public
 
 router.get('/handle/:handle', (req, res) => {
     const errors = {};
+  
     Profile.findOne({ handle: req.params.handle })
-        .populate('user', ['name', 'avatar'])
-        .then(profile => {
-            if(!profile) {
-                errors.noprofile = 'There is no profile for this user';
-                res.status(404).json(errors);
-            }
-
-            res.json(profile);
-        })
-        .catch(err => res.status(404).json(err));
-});
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+  
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  });
 
 // @route   GET api/profile/user/:user_id
 // @desc    Get profile by id
@@ -70,7 +90,7 @@ router.get('/user/:user_id', (req, res) => {
 
             res.json(profile);
         })
-        .catch(err => res.status(404).json(err));
+        .catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));
 });
 
 
